78298969cc70579a16924cc019599fdb8df24a54
Pooja Kumbharkar <pooja.kumbharkar@intel.com>
2025-09-15 11:10:36 +0530

    industrial-edge-insights-time-series: helm changes for sample apps
    Changes:
     1.package_helm.sh- Updated the files to copy
     2.Makefile- Added arg SAMPLE_APP for script package_helm.sh
     3.helm- Removed the dev vol mounts and added SAMPLE_APP env variable.
    
    Signed-off-by: Pooja Kumbharkar <pooja.kumbharkar@intel.com>

--- manufacturing-ai-suite/industrial-edge-insights-time-series/package_helm.sh
+++ manufacturing-ai-suite/industrial-edge-insights-time-series/package_helm.sh
@@ -5,15 +5,20 @@
 #
 #!/bin/bash -e
 
+SAMPLE_APP="${1:-wind-turbine-anomaly-detection}"
+echo "Packaging Helm chart for ${SAMPLE_APP}"
 cp -f grafana/dashboards/*.json helm/
 cp -f grafana/dashboards/*.yml helm/
+cp -f apps/${SAMPLE_APP}/grafana-dashboard/dashboard.json helm/
 cp -f influxdb/config/*.conf helm/
 cp -f influxdb/init-influxdb.sh helm/
 cp -f mqtt-broker/*.conf helm/
-cp -f simulator/simulation_data/windturbine_data.csv helm/
-cp -f telegraf/config/*.conf helm
+cp -f apps/${SAMPLE_APP}/ingestor-data/${SAMPLE_APP}.csv helm/
+cp -f apps/${SAMPLE_APP}/telegraf-config/*.conf helm
 cp -f grafana/entrypoint.sh helm/grafana_entrypoint.sh
-cp -f time_series_analytics_microservice/config.json helm/
+cp -f apps/${SAMPLE_APP}/time-series-analytics-config/config.json helm/
 cp -f telegraf/entrypoint.sh helm/telegraf_entrypoint.sh
 
-
+# Update the Chart.yaml and values.yaml with the SAMPLE_APP name
+sed -i "s/name: .*/name: ${SAMPLE_APP}-sample-app/" "helm/Chart.yaml"
+sed -i "s/SAMPLE_APP: .*/SAMPLE_APP: ${SAMPLE_APP}/" "helm/values.yaml"
