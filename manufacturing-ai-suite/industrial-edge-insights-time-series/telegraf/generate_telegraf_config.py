504c87c50c02efdecf68a8755f82f5a6661e970c
Pooja Kumbharkar <pooja.kumbharkar@intel.com>
2025-09-30 11:06:12 +0530

    industrial-edge-insights-time-series: Added benchmarking params to Makefile
    Changes:
     1.Makefile - Added ingestion interval and total number of points
     2.docker-compose.yml - Added env benchmarking
     3.telegraf/generate_telegraf_config.py - Update the ingestion interval
    
    Signed-off-by: Pooja Kumbharkar <pooja.kumbharkar@intel.com>

--- manufacturing-ai-suite/industrial-edge-insights-time-series/telegraf/generate_telegraf_config.py
+++ manufacturing-ai-suite/industrial-edge-insights-time-series/telegraf/generate_telegraf_config.py
@@ -44,6 +44,8 @@ def main():
     parser.add_argument("no_of_streams", type=int, help="Number of streams to generate", default=1)
     parser.add_argument("log_level", type=str, help="Log level", default="INFO")
     parser.add_argument("telegraf_metric_batch_size", type=int, help="Telegraf metric batch size", default=100)
+    parser.add_argument("ingestion_interval", type=str, help="Rate of ingestions like 1s, 100ms, 10ms, etc.,", default="1s")
+    parser.add_argument("ingestion_type", type=str, help="Type of ingestion (e.g., 'opcua' or 'mqtt')", default="opcua")
 
     # Parse the arguments
     args = parser.parse_args()
@@ -61,49 +63,51 @@ def main():
             content = content.replace("${DEBUG_MODE}", "true")
         # Now parse the content with replaced values
         config_data = tomlkit.parse(content)
-        stream_name = "opcua"
-        del config_data['inputs'][stream_name]
-        opc_array = []
-        for i in range(1, args.no_of_streams + 1):
-            opcua_section = tomlkit.table()
-            if args.no_of_streams == 1:
-                i = ""
-            opcua_section['name'] = f"opcua_stream_{i}"
-            opcua_section['name_override'] = "wind-turbine-data"
-            opcua_section['endpoint'] = f"opc.tcp://timeseriessoftware-ia-opcua-server-{i}:4840/freeopcua/server/"
-            if args.no_of_streams == 1:
-                opcua_section['name'] = f"opcua_stream"
-                opcua_section['endpoint'] = f"opc.tcp://timeseriessoftware-ia-opcua-server:4840/freeopcua/server/"
-            opcua_section['log_level'] = "debug"
-            opcua_section['auth_method'] = "Anonymous"
-            opcua_section['security_policy'] = "None"
-            opcua_section['security_mode'] = "None"
+        config_data['agent']['interval'] = args.ingestion_interval
+        if args.ingestion_type.lower() == "opcua":
+            stream_name = "opcua"
+            del config_data['inputs'][stream_name]
+            opc_array = []
+            for i in range(1, args.no_of_streams + 1):
+                opcua_section = tomlkit.table()
+                if args.no_of_streams == 1:
+                    i = ""
+                opcua_section['name'] = f"opcua_stream_{i}"
+                opcua_section['name_override'] = "wind-turbine-data"
+                opcua_section['endpoint'] = f"opc.tcp://timeseriessoftware-ia-opcua-server-{i}:4840/freeopcua/server/"
+                if args.no_of_streams == 1:
+                    opcua_section['name'] = f"opcua_stream"
+                    opcua_section['endpoint'] = f"opc.tcp://timeseriessoftware-ia-opcua-server:4840/freeopcua/server/"
+                opcua_section['log_level'] = "debug"
+                opcua_section['auth_method'] = "Anonymous"
+                opcua_section['security_policy'] = "None"
+                opcua_section['security_mode'] = "None"
 
-            nodes = []
+                nodes = []
 
-            node_table1 = tomlkit.table()
-            node_table1.add('name', "grid_active_power")
-            node_table1.add('namespace', "1")
-            node_table1.add('identifier_type', "i")
-            node_table1.add('identifier', "2002")
-            default_tags1 = tomlkit.inline_table()
-            default_tags1["source"] = f"opcua_merge{i}"
-            node_table1.add('default_tags', default_tags1)
-            nodes.append(node_table1)
+                node_table1 = tomlkit.table()
+                node_table1.add('name', "grid_active_power")
+                node_table1.add('namespace', "1")
+                node_table1.add('identifier_type', "i")
+                node_table1.add('identifier', "2002")
+                default_tags1 = tomlkit.inline_table()
+                default_tags1["source"] = f"opcua_merge{i}"
+                node_table1.add('default_tags', default_tags1)
+                nodes.append(node_table1)
 
-            node_table2 = tomlkit.table()
-            node_table2.add('name', "wind_speed")
-            node_table2.add('namespace', "1")
-            node_table2.add('identifier_type', "i")
-            node_table2.add('identifier', "2003")
-            default_tags2 = tomlkit.inline_table()
-            default_tags2["source"] = f"opcua_merge{i}"
-            node_table2.add('default_tags', default_tags2)
-            nodes.append(node_table2)
+                node_table2 = tomlkit.table()
+                node_table2.add('name', "wind_speed")
+                node_table2.add('namespace', "1")
+                node_table2.add('identifier_type', "i")
+                node_table2.add('identifier', "2003")
+                default_tags2 = tomlkit.inline_table()
+                default_tags2["source"] = f"opcua_merge{i}"
+                node_table2.add('default_tags', default_tags2)
+                nodes.append(node_table2)
 
-            opcua_section['nodes'] = nodes
-            opc_array.append(opcua_section)
-        config_data['inputs'][stream_name] = opc_array
+                opcua_section['nodes'] = nodes
+                opc_array.append(opcua_section)
+            config_data['inputs'][stream_name] = opc_array
 
         # Write the updated config directly to the output file
         with open(os.path.join(dir_path, "Telegraf_multi_stream.conf"), 'w') as file:
