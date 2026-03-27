de9bcbc46071045e8970f4ec369de11fce16be58
Pooja Kumbharkar <pooja.kumbharkar@intel.com>
2026-03-27 14:55:09 +0530

    timeseries: Fix helm test script
    Signed-off-by: Pooja Kumbharkar <pooja.kumbharkar@intel.com>

--- manufacturing-ai-suite/industrial-edge-insights-time-series/tests/functional/conftest_helm.py
+++ manufacturing-ai-suite/industrial-edge-insights-time-series/tests/functional/conftest_helm.py
@@ -8,6 +8,7 @@ import pytest
 import logging
 import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils import helm_utils
 from utils import constants
 import constants
